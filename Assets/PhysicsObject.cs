0f68e5f8877fc48b5d122c2218c9b08f66a65b2a
THAXTERIA-PC <39074621+SquallzFunk@users.noreply.github.com>
2019-10-11 23:53:40 -0400

    - Added some assets from unity store - Began work on the 2drigidbody gravity script

---
+++ Assets/PhysicsObject.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicsObject : MonoBehaviour
+{
+    public float gravityModifier = 1f;
+
+    protected Vector2 velocity;
+    protected Rigidbody2D rb;
+
+    void OnEnable()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+
+    void FixedUpdate()
+    {
+        velocity += gravityModifier * Physics2D.gravity * Time.deltaTime;
+
+        Vector2 deltaPosition = velocity * Time.deltaTime;
+
+        Vector2 move = Vector2.up * deltaPosition.y;
+
+        Movement(move);
+    }
+
+    void Movement(Vector2 move)
+    {
+        float distance = move.magnitude;
+        rb.position = rb.position + move;
+    }
+}
